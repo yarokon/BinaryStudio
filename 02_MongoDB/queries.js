07cd0e3e3b3e4737038074d0cd5f3f44285fd5c9
yarokon <yarokon1@gmail.com>
2017-05-29 00:26:46 +0300

    add MongoDB task

---
+++ 02_MongoDB/queries.js
@@ -0,0 +1,33 @@
+db.grades.find({
+  'scores': {
+    $elemMatch: {
+      score: {
+        $gt: 87,
+        $lt: 93
+      }
+    }
+  }
+});
+
+db.grades.aggregate(
+  [
+    {
+      $unwind : '$scores'
+    }, {
+      $match: {
+        'scores.type': 'exam',
+        'scores.score': {
+          $gt: 90
+        }
+      }
+    }
+  ]
+);
+
+db.grades.update({
+  name:'Dusti Lemmond'
+} ,{
+  $set: {'accepted': true}
+}, {
+  multi:true
+});
